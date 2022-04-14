c4a3ca5940f89c67ce3f71d63fa1e5f1343fdcbf
Ahmad-jama <m.jama221@gmail.com>
2022-04-14 23:00:10 +0300

    finish productmain component design

---
+++ src/components/HomeComponents/ShowProducts/ProductMain/Product/Product.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  AiOutlineStar,
+  AiOutlineEye,
+  AiOutlineHeart,
+  AiTwotoneHeart,
+} from "react-icons/ai";
+import { IoBagAddOutline } from "react-icons/io5";
+import styles from "./product.module.css";
+const Product = ({
+  id,
+  title,
+  type,
+  oldPrice,
+  newPrice,
+  ratings,
+  imageOne,
+  imageTwo,
+}) => {
+  const [like, setlike] = useState(true);
+  return (
+    <Link to={"/"} className={styles.product}>
+      <div className={styles.image_container}>
+        <img src={imageOne} alt={title} className={styles.image_one} />
+        <img src={imageTwo} alt={title} className={styles.image_two} />
+      </div>
+      <div className={styles.icon_box}>
+        <Link to={"/"}>
+          <AiOutlineEye />
+        </Link>
+        <span onClick={() => setlike(!like)}>
+          {like ? <AiOutlineHeart /> : <AiTwotoneHeart />}
+        </span>{" "}
+        <Link to={"/"}>
+          <IoBagAddOutline />
+        </Link>{" "}
+      </div>
+      <div className={styles.content}>
+        <span>{type}</span>
+        <p> {title} </p>
+        <div className={styles.ratings}>
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+          <AiOutlineStar />
+        </div>
+        <p>
+          {newPrice} <span> {oldPrice} </span>
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default Product;
--- src/components/HomeComponents/ShowProducts/ProductMain/ProductMain.js
+++ src/components/HomeComponents/ShowProducts/ProductMain/ProductMain.js
@@ -1,7 +1,141 @@
 import React from "react";
-
+import Product from "./Product/Product";
+import styles from "./product_main.module.css";
 const productMain = () => {
-  return <div>productMain</div>;
+  const productData = [
+    {
+      id: 1,
+      title: "Mens Winter Leathers Jackets",
+      type: "jaket",
+      oldPrice: "$32.00",
+      newPrice: "$45.00",
+      ratings: "3",
+      imageOne:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/jacket-4.jpg",
+      imageTwo:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/jacket-3.jpg",
+    },
+    {
+      id: 2,
+      title: "Pure Garment Dyed Cotton Shirt",
+      type: "SHIRT",
+      oldPrice: "$326.00",
+      newPrice: "$445.00",
+      ratings: "3",
+
+      imageOne:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/shirt-2.jpg",
+      imageTwo:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/shirt-1.jpg",
+    },
+    {
+      id: 3,
+      title: "MEN Yarn Fleece Full-Zip Jacket ",
+      type: "jaket",
+      oldPrice: "$102.00",
+      newPrice: "$153.00",
+      ratings: "3",
+      imageOne:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/jacket-6.jpg",
+      imageTwo:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/jacket-5.jpg",
+    },
+    {
+      id: 4,
+
+      title: "Black Floral Wrap Midi Skirt",
+      type: "skirt",
+      oldPrice: "$32.00",
+      newPrice: "$45.00",
+      ratings: "3",
+      imageOne:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/clothes-4.jpg",
+      imageTwo:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/clothes-3.jpg",
+    },
+    {
+      id: 5,
+      title: "Casual Men's Brown Shoes",
+      type: "CASUAL",
+      oldPrice: "$302.00",
+      newPrice: "$450.00",
+      ratings: "3",
+      imageOne:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/shoe-2_1.jpg",
+      imageTwo:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/shoe-2.jpg",
+    },
+    {
+      id: 6,
+      title: " Pocket Watch Leather Pouch",
+      type: "WATCHES",
+      oldPrice: "$32.00",
+      newPrice: "$45.00",
+      ratings: "3",
+      imageOne:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/watch-4.jpg",
+      imageTwo:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/watch-3.jpg",
+    },
+    {
+      id: 7,
+      title: "  Womens Party Wear Shoes",
+      type: "PARTY WEAR",
+      oldPrice: "$32.00",
+      newPrice: "$45.00",
+      ratings: "3",
+      imageOne:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/party-wear-2.jpg",
+      imageTwo:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/party-wear-1.jpg",
+    },
+    {
+      id: 8,
+      title: " Better Basics French Terry Sweatshorts",
+      type: "SHORTS",
+      oldPrice: "$32.00",
+      newPrice: "$45.00",
+      ratings: "3",
+
+      imageOne:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/shorts-1.jpg",
+      imageTwo:
+        "https://codewithsadee.github.io/anon-ecommerce-website/assets/images/products/shorts-2.jpg",
+    },
+  ];
+
+  return (
+    <div className={styles.productsmain}>
+      <h2>New Products</h2>
+      <div className={styles.products_container}>
+        {productData.map((product) => {
+          const {
+            id,
+            title,
+            type,
+            oldPrice,
+            newPrice,
+            ratings,
+            imageOne,
+            imageTwo,
+          } = product;
+          return (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              type={type}
+              ratings={ratings}
+              oldPrice={oldPrice}
+              newPrice={newPrice}
+              imageOne={imageOne}
+              imageTwo={imageTwo}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default productMain;
